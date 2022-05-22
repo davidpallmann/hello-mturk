18f8ef56ad70ee9d5b2f136095f650d83cba99f7
David Pallmann <dpallmann@hotmail.com>
2022-05-22 17:22:54 -0500

    Initial code

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Amazon.MTurk;
+using Amazon.MTurk.Model;
+
+string SANDBOX_URL = "https://mturk-requester-sandbox.us-east-1.amazonaws.com";
+//string PROD_URL = "https://mturk-requester.us-east-1.amazonaws.com";
+
+var config = new AmazonMTurkConfig()
+{
+    RegionEndpoint = Amazon.RegionEndpoint.USEast1,
+    ServiceURL = SANDBOX_URL // PROD_URL
+};
+
+var mturkClient = new AmazonMTurkClient(config);
+
+var command = (args.Length > 0) ? args[0] : null;
+
+if (command == "balance")
+{
+    GetAccountBalanceRequest request = new GetAccountBalanceRequest();
+    GetAccountBalanceResponse balance = await mturkClient.GetAccountBalanceAsync(request);
+    Console.WriteLine("Your account balance is $" + balance.AvailableBalance);
+}
+else if (command == "add")
+{
+    string questionXML = System.IO.File.ReadAllText(@"question.xml");
+
+    var hitRequest = new CreateHITRequest()
+    {
+        LifetimeInSeconds = 60*60*3,
+        AssignmentDurationInSeconds = 60*3,
+        Reward = "0.03",
+        Title = "Type the (one or two words) you see in the image",
+        Description = "Enter the highlighted text in the oval",
+        Question = questionXML,
+        MaxAssignments = 3
+    };
+
+    var createResponse = mturkClient.CreateHITAsync(hitRequest);
+    var HITId = createResponse.Result.HIT.HITId;
+    Console.WriteLine($"HIT created - Id: {HITId}");
+    Console.WriteLine($"Worker link: https://workersandbox.mturk.com/projects/{createResponse.Result.HIT.HITTypeId}/tasks");
+
+    var cont = String.Empty;
+    Console.WriteLine("Press any key to update, or Q to quit.");
+    do
+    {
+        var statusResponse = mturkClient.GetHITAsync(new GetHITRequest { HITId = HITId });
+        var HIT = statusResponse.Result.HIT;
+        Console.WriteLine($"Status: {HIT.HITStatus}, Expiration: {HIT.Expiration}, Available: {HIT.NumberOfAssignmentsAvailable}, Pending: {HIT.NumberOfAssignmentsPending}, Completed: {HIT.NumberOfAssignmentsCompleted}");
+    } while (Console.ReadKey().KeyChar != 'Q');
+
+}
+else if (command=="results")
+{
+    var listRequest = new ListAssignmentsForHITRequest()
+    {
+        HITId = args[1]
+    };
+    var listResponse = await mturkClient.ListAssignmentsForHITAsync(listRequest);
+    foreach(var assignment in listResponse.Assignments)
+    {
+        Console.WriteLine($"Worker Id: {assignment.WorkerId}, Answer: {assignment.Answer}");
+    }
+}
+else
+{
+    Console.WriteLine("dotnet run -- add ................ creates a new Human Intelligence Task and monitors status");
+    Console.WriteLine("dotnet run -- balance ............ shows account balance");
+    Console.WriteLine("dotnet run -- results [hit-id] ... shows results for HIT");
+}
+
+
+
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello, Mechanical Turk!
+
+This is the code project for the [Hello, Mechanical Turk!](https://davidpallmann.hashnode.dev/hello-mechanical-turk) blog post. 
+
+This episode: Amazon Mechanical Turk and crowd-sourcing human intelligence tasks. In this Hello, Cloud blog series, we're covering the basics of AWS cloud services for newcomers who are .NET developers. If you love C# but are new to AWS, or to this particular service, this should give you a jumpstart.
+
+In this post we'll introduce Amazon Mechanical Turk and use it in a "Hello, Cloud" .NET program to crowd-source human intelligence tasks. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 6.
+
+## Our Hello, Mechanical Turk Project
+
+We will write a .NET program that can create HITs and get results using the AWS SDK for .NET. The HIT we'll create will ask workers what is their favorite flavor of ice cream. We'll run this in the MTurk developer sandbox to avoid any charges.
+
+See the blog post for the tutorial to create this project and run it on AWS.
+
